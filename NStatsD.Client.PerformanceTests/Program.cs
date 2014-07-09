2adb0b13f458b559a7f5b2a071ddf8006602e70e
Aaron Stannard <aaron@markedup.com>
2014-07-08 23:13:42 -0700

    added a performance test

---
+++ NStatsD.Client.PerformanceTests/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NStatsD;
+
+namespace PerformanceTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We're going to write a ton of data to the console. 10 million iterations.");
+            Console.WriteLine("Going!");
+            var i = 0;
+            while(i < 10000000)
+            {
+                Client.Current.Increment("performancetest.increment");
+                i++;
+            }
+            Console.WriteLine("Done, press any key to exit");
+            Console.ReadKey();
+            return;
+        }
+    }
+}
